3f4d15a440f2c2cd22d2ef2bf399af8b2c3d1877
Maximilian Steuck <max@fount.io>
2021-11-15 17:01:57 +0100

    Create Models, Migrations, Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :courses, only: %i[index show new create] do
+    resources :bookings, only: %i[new create]
+  end
+
+  resource :dashboard, only: [:show]
 end
---
+++ db/migrate/20211115155640_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :course, null: false, foreign_key: true
+      t.integer :status
+      t.integer :total_price
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
