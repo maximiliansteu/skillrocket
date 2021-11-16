b69b5004ac279dae042b2b057707cfae64d0fdd5
Oyvind Fossum <fooy1@pm.me>
2021-11-16 11:26:03 +0100

    create method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,20 @@
 class CoursesController < ApplicationController
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description, :price, :content, :category, :location, :user_id, :photo)
+  end
 end
