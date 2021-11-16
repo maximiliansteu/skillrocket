f4704ca48d0c7b37813f847215221e655d688335
Maximilian Steuck <max@fount.io>
2021-11-16 18:03:39 +0100

    Featur: Created Show in Booking as well as some Booking actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @course = Course.find(params[:course_id])
+
+    @booking.course = @course
+    if @booking.save!
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit()
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,11 +3,11 @@ class Booking < ApplicationRecord
   belongs_to :course
   enum status: [:pending, :confirmed, :canceled, :done]
 
-  # before_create :set_total_price
+  before_create :set_total_price
 
-  # private
+  private
 
-  # def set_total_price
-  #   self.total_price = (end_date - start_date).to_i * course.price
-  # end
+  def set_total_price
+    self.total_price = (end_date - start_date).to_i * course.price
+  end
 end
