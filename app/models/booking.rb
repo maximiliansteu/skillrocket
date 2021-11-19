7513254968a206836b28e9c330fdcfabc67391d1
javiersuay <jota.suay@gmail.com>
2021-11-19 11:04:36 +0100

    fixed seed error (table dependencies in bookings, course and reviews

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :course
-  has_one :review
+  has_one :review, dependent: :destroy
   enum status: [:pending, :confirmed, :canceled, :done]
 
   before_create :set_total_price
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 
   CATEGORIES = ["Sports",
